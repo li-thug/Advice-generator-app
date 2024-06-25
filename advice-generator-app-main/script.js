2e6acddd1aa6ad89eb797a10a794e4171be2246a
li-thug <akinbinuemmanuel04@gmail.com>
2024-06-25 11:35:35 +0100

    second commit

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -3,21 +3,21 @@ const adviceNumber = document.querySelector(".advice-number");
 const adviceQuote = document.querySelector(".advice-quote");
 
 async function fetchAdvice() {
-    try {
-        const response = await fetch("https://api.adviceslip.com/advice");
-        const responseJson = await response.json();
-        return responseJson;
-    } catch(e) {
-        console.error(e);
-    }
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const responseJson = await response.json();
+    return responseJson;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 async function replaceContent() {
-    const response = await fetchAdvice();
-    adviceNumber.innerText = response.slip.id;
-    adviceQuote.innerText = `${response.slip.advice}`;
+  const response = await fetchAdvice();
+  adviceNumber.innerText = response.advices.id;
+  adviceQuote.innerText = `${response.advices.advice}`;
 }
 
 replaceContent();
 
-adviceButton.addEventListener("click", replaceContent);
+adviceButton.addEventListener(replaceContent);
